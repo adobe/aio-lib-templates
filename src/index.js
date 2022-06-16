454e2415e5ce363cedc08d01c505e3ed6dbad104
Serhii Litviachenko <slitviachenko@gmail.com>
2022-06-16 18:34:56 -0500

    Add constants

--- src/index.js
+++ src/index.js
@@ -14,6 +14,30 @@ require('./types.jsdoc'); // for VS Code autocomplete
 const logger = require('@adobe/aio-lib-core-logging')('@adobe/aio-lib-templates:index', { 'level': process.env.LOG_LEVEL });
 const TemplateRegistryAPI = require('./TemplateRegistryAPI');
 
+const SEARCH_CRITERIA_NAMES = 'names';
+const SEARCH_CRITERIA_CATEGORIES = 'categories';
+const SEARCH_CRITERIA_STATUSES = 'statuses';
+const SEARCH_CRITERIA_APIS = 'apis';
+const SEARCH_CRITERIA_EXTENSIONS = 'extensions';
+const SEARCH_CRITERIA_EVENTS = 'events';
+const SEARCH_CRITERIA_RUNTIME = 'runtime';
+const SEARCH_CRITERIA_ADOBE_RECOMMENDED = 'adobeRecommended';
+
+const SEARCH_CRITERIA_FILTER_ANY = '*';
+const SEARCH_CRITERIA_FILTER_NONE = '';
+
+const ORDER_BY_CRITERIA_NAMES = 'names';
+const ORDER_BY_CRITERIA_STATUSES = 'statuses';
+const ORDER_BY_CRITERIA_ADOBE_RECOMMENDED = 'adobeRecommended';
+const ORDER_BY_CRITERIA_PUBLISH_DATE = 'publishDate';
+
+const ORDER_BY_CRITERIA_SORT_DESC = 'desc';
+const ORDER_BY_CRITERIA_SORT_ASC = 'asc';
+
+const TEMPLATE_STATUS_IN_VERIFICATION = 'InVerification';
+const TEMPLATE_STATUS_APPROVED = 'Approved';
+const TEMPLATE_STATUS_REJECTED = 'Rejected';
+
 /**
  * Returns a new TemplateRegistryAPI object.
  *
@@ -26,5 +50,24 @@ function init(config = {}) {
 }
 
 module.exports = {
+    SEARCH_CRITERIA_NAMES,
+    SEARCH_CRITERIA_CATEGORIES,
+    SEARCH_CRITERIA_STATUSES,
+    SEARCH_CRITERIA_APIS,
+    SEARCH_CRITERIA_EXTENSIONS,
+    SEARCH_CRITERIA_EVENTS,
+    SEARCH_CRITERIA_RUNTIME,
+    SEARCH_CRITERIA_ADOBE_RECOMMENDED,
+    SEARCH_CRITERIA_FILTER_ANY,
+    SEARCH_CRITERIA_FILTER_NONE,
+    ORDER_BY_CRITERIA_NAMES,
+    ORDER_BY_CRITERIA_STATUSES,
+    ORDER_BY_CRITERIA_ADOBE_RECOMMENDED,
+    ORDER_BY_CRITERIA_PUBLISH_DATE,
+    ORDER_BY_CRITERIA_SORT_DESC,
+    ORDER_BY_CRITERIA_SORT_ASC,
+    TEMPLATE_STATUS_IN_VERIFICATION,
+    TEMPLATE_STATUS_APPROVED,
+    TEMPLATE_STATUS_REJECTED,
     init
 };
